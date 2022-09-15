5c3898f2580c2c93793f7d27c313a37fcca3b88d
Ana Pao Minchaca <42215143+anapao-minchaca@users.noreply.github.com>
2022-09-14 20:06:48 -0500

    Create 03_Liskov.py

---
+++ 03_Liskov.py
@@ -0,0 +1,47 @@
+# Establece que una clase secundaria debe ser sustituible por su clase principal
+# El principio de sustitución de Liskov tiene como objetivo garantizar que la
+# _ clase secundaria pueda asumir el lugar de su clase principal sin causar ningún error
+
+# importación de la herencia y un método abstracto
+from abc import ABC, abstractmethod
+
+class Notification(ABC):
+    @abstractmethod
+    def notify(self, message, email):
+        pass
+
+class Email(Notification):
+    def notify(self, message, email):
+        print(f'send {message} to {email}')
+
+class Text(Notification):
+    def notify(self, message, email):
+        print(f'send {message} to {email}')
+
+class Contact:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        pass
+
+class NotificationManager:
+    def __init__(self, notification, contact):
+        self.contact = contact
+        self.notification = notification
+
+    def send(self, message):
+        if isinstance(self.notification, Email):
+            self.notification.notify(message, contact.email)
+        elif isinstance(self.notification, Text):
+            self.notification.notify(message, contact.email)
+        else:
+            raise Exception ('La notificación que nos enviaste no está disponible aún')
+
+if __name__ == '__main__':
+    # _notification = Email()
+    # _notification.notify("hola", "anapissy75@gmail.com")
+
+    contact = Contact("Ana Paola", "anapissy75@gmail.com", "55523464")
+    not_manager = NotificationManager(Text(), contact)
+    not_manager.send("Hola =>")
