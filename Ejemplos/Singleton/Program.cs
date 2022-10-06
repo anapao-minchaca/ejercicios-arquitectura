0d77178dbabc4b4e4d9973fcff5d342ee92502b8
Ana Pao Minchaca <42215143+anapao-minchaca@users.noreply.github.com>
2022-10-05 20:32:55 -0500

    update

---
+++ Ejemplos/Singleton/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// Singleton pattern -- Ejemplo estructural
+// dotnet run
+
+namespace Patrones.Singleton
+{
+
+  class MainApp
+  {
+
+    static void Main()
+    {
+
+      Singleton s1 = Singleton.Instance();
+
+      Singleton s2 = Singleton.Instance();
+
+
+      if (s1 == s2)
+      {
+        Console.WriteLine("Son iguales");
+      }
+
+      Console.ReadKey();
+
+    }
+
+  }
+
+
+  class Singleton
+  {
+    private static Singleton _instance;
+
+ 
+    protected Singleton()
+    {
+    }
+
+ 
+    public static Singleton Instance()
+    {
+      if (_instance == null)
+      {
+        _instance = new Singleton();
+      }
+
+      return _instance;
+
+    }
+
+  }
+
+}
