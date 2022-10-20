6e29249fc07bb374755ee5aa30cdc2bb68f7a046
Ana Pao Minchaca <42215143+anapao-minchaca@users.noreply.github.com>
2022-10-19 21:27:29 -0500

    update abstract

--- 191022/_abstract_/Program.cs
+++ 191022/_abstract_/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Abstract Factory es un patrón de diseño creacional que nos permite producir 
 // familias de objetos relacionados sin especificar sus clases concretas.
 
+// La diferencia es que el factory solo puede crear una clase con ciertas descripciones mientras
+// que el abstract puede crear multiples objetos con las mismas descripciones
+
 using System;
 
 // Abstract Factory -- Ejemplo estructural
